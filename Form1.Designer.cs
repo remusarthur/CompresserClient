cdbd45660109d13059383341e0a588d9b5681e3b
remus.arthur <remus.arthur@gmail.com>
2015-05-07 20:04:17 +0300

    Init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿using System.Windows.Forms.DataVisualization.Charting;
+namespace CompresserClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.button1 = new System.Windows.Forms.Button();
+			this.checkboxLZW = new System.Windows.Forms.CheckBox();
+			this.checkboxRLE = new System.Windows.Forms.CheckBox();
+			this.checkboxMULAW = new System.Windows.Forms.CheckBox();
+			this.checkboxSMART = new System.Windows.Forms.CheckBox();
+			this.radioButtonTime = new System.Windows.Forms.RadioButton();
+			this.radioButtonSize = new System.Windows.Forms.RadioButton();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// chart1
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			legend1.Name = "Legend1";
+			this.chart1.Legends.Add(legend1);
+			this.chart1.Location = new System.Drawing.Point(12, 12);
+			this.chart1.Name = "chart1";
+			series1.BorderWidth = 3;
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series1.Legend = "Legend1";
+			series1.Name = "RLE";
+			series2.BorderWidth = 3;
+			series2.ChartArea = "ChartArea1";
+			series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series2.Legend = "Legend1";
+			series2.Name = "LZW";
+			series3.BorderWidth = 3;
+			series3.ChartArea = "ChartArea1";
+			series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series3.Color = System.Drawing.Color.ForestGreen;
+			series3.Legend = "Legend1";
+			series3.Name = "SMART";
+			series4.BorderWidth = 3;
+			series4.ChartArea = "ChartArea1";
+			series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series4.Legend = "Legend1";
+			series4.Name = "MULAW";
+			this.chart1.Series.Add(series1);
+			this.chart1.Series.Add(series2);
+			this.chart1.Series.Add(series3);
+			this.chart1.Series.Add(series4);
+			this.chart1.Size = new System.Drawing.Size(1100, 800);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			this.chart1.Click += new System.EventHandler(this.chart1_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(992, 158);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// checkboxLZW
+			// 
+			this.checkboxLZW.AutoSize = true;
+			this.checkboxLZW.Location = new System.Drawing.Point(987, 217);
+			this.checkboxLZW.Name = "checkboxLZW";
+			this.checkboxLZW.Size = new System.Drawing.Size(50, 17);
+			this.checkboxLZW.TabIndex = 2;
+			this.checkboxLZW.Text = "LZW";
+			this.checkboxLZW.UseVisualStyleBackColor = true;
+			this.checkboxLZW.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// checkboxRLE
+			// 
+			this.checkboxRLE.AutoSize = true;
+			this.checkboxRLE.Location = new System.Drawing.Point(987, 240);
+			this.checkboxRLE.Name = "checkboxRLE";
+			this.checkboxRLE.Size = new System.Drawing.Size(47, 17);
+			this.checkboxRLE.TabIndex = 3;
+			this.checkboxRLE.Text = "RLE";
+			this.checkboxRLE.UseVisualStyleBackColor = true;
+			// 
+			// checkboxMULAW
+			// 
+			this.checkboxMULAW.AutoSize = true;
+			this.checkboxMULAW.Location = new System.Drawing.Point(987, 263);
+			this.checkboxMULAW.Name = "checkboxMULAW";
+			this.checkboxMULAW.Size = new System.Drawing.Size(59, 17);
+			this.checkboxMULAW.TabIndex = 4;
+			this.checkboxMULAW.Text = "MLAW";
+			this.checkboxMULAW.UseVisualStyleBackColor = true;
+			// 
+			// checkboxSMART
+			// 
+			this.checkboxSMART.AutoSize = true;
+			this.checkboxSMART.Location = new System.Drawing.Point(987, 286);
+			this.checkboxSMART.Name = "checkboxSMART";
+			this.checkboxSMART.Size = new System.Drawing.Size(64, 17);
+			this.checkboxSMART.TabIndex = 5;
+			this.checkboxSMART.Text = "SMART";
+			this.checkboxSMART.UseVisualStyleBackColor = true;
+			// 
+			// radioButtonTime
+			// 
+			this.radioButtonTime.AutoSize = true;
+			this.radioButtonTime.Checked = true;
+			this.radioButtonTime.Location = new System.Drawing.Point(6, 30);
+			this.radioButtonTime.Name = "radioButtonTime";
+			this.radioButtonTime.Size = new System.Drawing.Size(48, 17);
+			this.radioButtonTime.TabIndex = 9;
+			this.radioButtonTime.TabStop = true;
+			this.radioButtonTime.Text = "Time";
+			this.radioButtonTime.UseVisualStyleBackColor = true;
+			// 
+			// radioButtonSize
+			// 
+			this.radioButtonSize.AutoSize = true;
+			this.radioButtonSize.Location = new System.Drawing.Point(6, 53);
+			this.radioButtonSize.Name = "radioButtonSize";
+			this.radioButtonSize.Size = new System.Drawing.Size(45, 17);
+			this.radioButtonSize.TabIndex = 10;
+			this.radioButtonSize.Text = "Size";
+			this.radioButtonSize.UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.radioButtonTime);
+			this.groupBox1.Controls.Add(this.radioButtonSize);
+			this.groupBox1.Location = new System.Drawing.Point(975, 356);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(92, 100);
+			this.groupBox1.TabIndex = 11;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Measure";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1079, 590);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.checkboxSMART);
+			this.Controls.Add(this.checkboxMULAW);
+			this.Controls.Add(this.checkboxRLE);
+			this.Controls.Add(this.checkboxLZW);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.chart1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.CheckBox checkboxLZW;
+		private System.Windows.Forms.CheckBox checkboxRLE;
+		private System.Windows.Forms.CheckBox checkboxMULAW;
+		private System.Windows.Forms.CheckBox checkboxSMART;
+		private System.Windows.Forms.RadioButton radioButtonTime;
+		private System.Windows.Forms.RadioButton radioButtonSize;
+		private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace CompresserClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+			Enable_Zooming();
+        }
+
+		void Enable_Zooming()
+		{
+			this.MouseWheel += new MouseEventHandler(Form1_MouseWheel);
+			// Set automatic zooming
+			chart1.ChartAreas["ChartArea1"].AxisX.ScaleView.Zoomable = true;
+			chart1.ChartAreas["ChartArea1"].AxisY.ScaleView.Zoomable = true;
+
+			// Set automatic scrolling 
+			chart1.ChartAreas["ChartArea1"].CursorX.AutoScroll = true;
+			chart1.ChartAreas["ChartArea1"].CursorY.AutoScroll = true;
+
+			// Allow user selection for Zoom
+			chart1.ChartAreas["ChartArea1"].CursorX.IsUserSelectionEnabled = true;
+			chart1.ChartAreas["ChartArea1"].CursorY.IsUserSelectionEnabled = true;
+
+			chart1.ChartAreas["ChartArea1"].AxisX.ScaleView.Zoomable = true;
+			chart1.ChartAreas["ChartArea1"].AxisY.ScaleView.Zoomable = true;
+
+			//chart.MouseWheel += new MouseEventHandler(chart_MouseWheel);
+		}
+
+		void Form1_MouseWheel(object sender, MouseEventArgs e)
+		{
+
+			try
+			{
+				if (e.Delta > 0)
+				{
+					double xMin = chart1.ChartAreas["ChartArea1"].AxisX.ScaleView.ViewMinimum;
+					double xMax = chart1.ChartAreas["ChartArea1"].AxisX.ScaleView.ViewMaximum;
+					double yMin = chart1.ChartAreas["ChartArea1"].AxisY.ScaleView.ViewMinimum;
+					double yMax = chart1.ChartAreas["ChartArea1"].AxisY.ScaleView.ViewMaximum;
+
+					double posXStart = chart1.ChartAreas["ChartArea1"].AxisX.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 2;
+					double posXFinish = chart1.ChartAreas["ChartArea1"].AxisX.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 2;
+					double posYStart = chart1.ChartAreas["ChartArea1"].AxisY.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / 2;
+					double posYFinish = chart1.ChartAreas["ChartArea1"].AxisY.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / 2;
+
+					chart1.ChartAreas["ChartArea1"].AxisX.ScaleView.Zoom(posXStart, posXFinish);
+					chart1.ChartAreas["ChartArea1"].AxisY.ScaleView.Zoom(posYStart, posYFinish);
+				}
+				else if (e.Delta < 0)
+				{
+					ZoomOut();
+				}
+
+			}
+			catch { }
+		}
+
+		private void ZoomOut()
+		{
+			chart1.ChartAreas["ChartArea1"].AxisX.ScaleView.ZoomReset();
+			chart1.ChartAreas["ChartArea1"].AxisY.ScaleView.ZoomReset();
+		}
+
+		/*
+		 * 
+		 * 
+		 * Here it starts the compresser Client
+		 * 
+		 * 
+		 * */
+
+		List<CompPoint> points = new List<CompPoint>();
+
+		class CompPoint : IComparable<CompPoint>
+		{
+			public double x;
+			public double y;
+			public SCClient.Mode mode;
+
+			public int CompareTo(CompPoint other)
+			{
+				if (x == other.x)
+				{
+					if (y == other.y)
+					{
+						return mode.CompareTo(other.mode);
+					}
+					else
+						return y.CompareTo(other.y);
+				}
+				else
+					return x.CompareTo(other.x);
+			}
+		};
+
+		private void BindPoints()
+		{
+			points.Sort();
+
+			foreach(SCClient.Mode mode in Enum.GetValues(typeof(SCClient.Mode)))
+				chart1.Series[mode.ToString()].Points.Clear();
+
+			foreach (CompPoint p in points)
+			{
+				chart1.Series[p.mode.ToString()].Points.AddXY(p.x, p.y);
+			}
+		}
+
+		class SCClient
+		{
+			public enum Mode
+			{
+				LZW = 1,
+				RLE = 2,
+				MULAW = 4,
+				SMART = 8
+			};
+
+			static Random rnd = new Random();
+			private static CompPoint GetRandomPoint()
+			{
+				CompPoint p = new CompPoint();
+				p.x = rnd.NextDouble() * 100;
+				p.y = rnd.NextDouble() * p.x;
+				Thread.Sleep(1000);
+				return p;
+			}
+
+			private static void CompressFile(string fileName, int mode, IProgress<CompPoint> progress, bool measureTime)
+			{
+				foreach (Mode bitMode in Enum.GetValues(typeof(Mode)))
+				{
+					if ((mode & (int)bitMode) == 0)
+						continue;
+
+					//start process that compresses the file
+					Process process = new Process();
+					string outputName = fileName + "." + bitMode.ToString() + ".scformat";
+					process.StartInfo.FileName = "C:\\Users\\Remus\\Documents\\Visual Studio 2013\\Projects\\SmartCompresser\\Release\\smartCompresser.exe";
+					process.StartInfo.Arguments = "\"" + fileName + "\" \"" + outputName + "\" " + bitMode.ToString();
+					process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+					process.Start();
+
+					long startTime = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+					process.WaitForExit();
+					long endTime = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+					CompPoint point = new CompPoint();
+
+					try
+					{
+						long initialLength = new System.IO.FileInfo(fileName).Length;
+						long compressedLength = new System.IO.FileInfo(outputName).Length;
+						point.x = initialLength;
+						point.y = compressedLength;
+						point.mode = bitMode;
+					}
+					catch (Exception e)
+					{
+						;
+					}
+
+					if (measureTime == true)
+					{
+						point.y = endTime - startTime;
+					}
+
+					progress.Report(point);
+				}
+
+			}
+
+			public static void CompressFiles(string targetDirectory, int mode, IProgress<CompPoint> progress, bool measureTime)
+			{
+				string[] fileEntries = Directory.GetFiles(targetDirectory);
+				foreach (string fileName in fileEntries)
+					CompressFile(fileName, mode, progress, measureTime);
+			}
+		}
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+			string targetDirectory = "D:\\SCDB\\";
+			int mode = 0;
+			if(checkboxLZW.Checked)
+				mode |= (int)SCClient.Mode.LZW;
+			if(checkboxMULAW.Checked)
+				mode |= (int)SCClient.Mode.MULAW;
+			if(checkboxRLE.Checked)
+				mode |= (int)SCClient.Mode.RLE;
+			if (checkboxSMART.Checked)
+				mode |= (int)SCClient.Mode.SMART;
+
+			points.Clear();
+			var progress = new Progress<CompPoint>(newP => { points.Add(newP); BindPoints(); });
+			
+			await Task.Factory.StartNew(() => SCClient.CompressFiles(targetDirectory, mode, progress, radioButtonTime.Checked),
+										TaskCreationOptions.LongRunning);
+        }
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void chart1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		
+ 
+    }
+}
